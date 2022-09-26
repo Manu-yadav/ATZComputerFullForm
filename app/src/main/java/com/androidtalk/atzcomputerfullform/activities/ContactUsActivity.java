72a3900f4394e25ba55a6b9dfddc955ca1b8c8f4
Manoj Kumar <manu.madstech@gmail.com>
2022-09-26 23:27:36 +0530

    Added abbreviation for medical (A-M) and ContactUsActivity

---
+++ app/src/main/java/com/androidtalk/atzcomputerfullform/activities/ContactUsActivity.java
@@ -0,0 +1,28 @@
+package com.androidtalk.atzcomputerfullform.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.androidtalk.atzcomputerfullform.R;
+
+public class ContactUsActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_us);
+        initView();
+        initVariables();
+    }
+
+    @Override
+    void initView() {
+
+    }
+
+    @Override
+    void initVariables() {
+
+    }
+}
